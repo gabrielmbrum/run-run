9a4b19a00fba331455a1aebfd9afa621cf9fd40c
Brum <gabrielbrum2805@gmail.com>
2024-07-15 23:08:27 -0300

    commenting the functions

--- ui.h
+++ ui.h
@@ -3,7 +3,10 @@
 
 #include "mapa.h"
 
+// print the line of index 'parte' of the matriz 'desenho'
 void printpart(char desenho[4][7], int parte);
+
+// controls the logic of printing the map with the paints of each part of the map
 void printmap(Mapa m);
 
 #endif
